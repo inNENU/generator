73dfbf906534ae0fe4a2fdb14965aba92ed74c8c
Mr.Hope <mister-hope@outlook.com>
2025-02-19 18:44:10 +0800

    chore: update linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,13 @@
-import hopeConfig, { config, tsParser } from "eslint-config-mister-hope";
+import { hope } from "eslint-config-mister-hope";
 
-export default config(
-  ...hopeConfig,
-  {
-    ignores: ["**/node_modules/**", "coverage/**", "dist/**", "typings.d.ts"],
-  },
-
-  {
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      parserOptions: {
-        parser: tsParser,
-        tsconfigDirName: import.meta.dirname,
-        projectService: {
-          allowDefaultProject: ["eslint.config.js", "rollup.config.ts"],
-        },
+export default hope({
+  ignores: ["typings.d.ts"],
+  ts: {
+    parserOptions: {
+      projectService: {
+        allowDefaultProject: ["eslint.config.js", "rollup.config.ts"],
       },
     },
-  },
-
-  {
-    files: ["**/*.ts"],
     rules: {
       "@typescript-eslint/naming-convention": [
         "warn",
@@ -63,6 +49,7 @@ export default config(
         },
       ],
       "@typescript-eslint/no-unnecessary-condition": "off",
+      "no-console": "off",
     },
   },
-);
+});
--- src/components/phone/json.ts
+++ src/components/phone/json.ts
@@ -9,7 +9,7 @@ export const getPhoneJSON = (
   for (const key in phone)
     if (typeof phone[key as keyof PhoneComponentOptions] === "number")
       // @ts-expect-error: Type does not fit
-      // eslint-disable-next-line
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
       phone[key] = phone[key].toString();
 
   checkKeys(
