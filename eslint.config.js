79dbad5a898d1754d4ef346c73b68e580edadbe0
Mister-Hope <mister-hope@outlook.com>
2025-04-16 13:00:16 +0800

    build: bump deps

--- eslint.config.js
+++ eslint.config.js
@@ -2,54 +2,56 @@ import { hope } from "eslint-config-mister-hope";
 
 export default hope({
   ignores: ["typings.d.ts"],
-  ts: {
+
+  languageOptions: {
     parserOptions: {
       projectService: {
         allowDefaultProject: ["eslint.config.js", "rollup.config.ts"],
       },
     },
-    rules: {
-      "@typescript-eslint/naming-convention": [
-        "warn",
-        {
-          selector: "default",
-          format: ["camelCase"],
-          leadingUnderscore: "allowSingleOrDouble",
-          trailingUnderscore: "allow",
-        },
-        {
-          selector: ["variable"],
-          format: ["camelCase", "PascalCase", "UPPER_CASE"],
-          leadingUnderscore: "allowSingleOrDouble",
-          trailingUnderscore: "allowSingleOrDouble",
-        },
-        {
-          selector: ["parameter"],
-          format: ["camelCase", "PascalCase"],
-          leadingUnderscore: "allow",
-          trailingUnderscore: "allow",
-        },
-        {
-          selector: ["property"],
-          format: ["camelCase", "PascalCase", "UPPER_CASE"],
-          leadingUnderscore: "allow",
-          trailingUnderscore: "allow",
-        },
-        {
-          selector: "import",
-          format: ["PascalCase", "camelCase"],
-        },
-        {
-          selector: "typeLike",
-          format: ["PascalCase"],
-        },
-        {
-          selector: "enumMember",
-          format: ["PascalCase"],
-        },
-      ],
-      "@typescript-eslint/no-unnecessary-condition": "off",
-      "no-console": "off",
-    },
+  },
+
+  ts: {
+    "@typescript-eslint/naming-convention": [
+      "warn",
+      {
+        selector: "default",
+        format: ["camelCase"],
+        leadingUnderscore: "allowSingleOrDouble",
+        trailingUnderscore: "allow",
+      },
+      {
+        selector: ["variable"],
+        format: ["camelCase", "PascalCase", "UPPER_CASE"],
+        leadingUnderscore: "allowSingleOrDouble",
+        trailingUnderscore: "allowSingleOrDouble",
+      },
+      {
+        selector: ["parameter"],
+        format: ["camelCase", "PascalCase"],
+        leadingUnderscore: "allow",
+        trailingUnderscore: "allow",
+      },
+      {
+        selector: ["property"],
+        format: ["camelCase", "PascalCase", "UPPER_CASE"],
+        leadingUnderscore: "allow",
+        trailingUnderscore: "allow",
+      },
+      {
+        selector: "import",
+        format: ["PascalCase", "camelCase"],
+      },
+      {
+        selector: "typeLike",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "enumMember",
+        format: ["PascalCase"],
+      },
+    ],
+    "@typescript-eslint/no-unnecessary-condition": "off",
+    "no-console": "off",
   },
 });
